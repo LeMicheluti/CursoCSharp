1b4f741b7acfd95e00a5d73ab4f27fef92047a9a
LeMicheluti <lemicheluti@gmail.com>
2019-07-31 06:42:40 -0300

    Comentários

---
+++ CursoCSharp/Fundamentos/Comentários.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos {
+    /// <summary>
+    /// Exemplos de comentários
+    /// </summary>
+    class Comentários {
+        public static void Executar() {
+            // Cuidado com os comentários desnecessários
+            Console.WriteLine("Código claro é sempre melhor!");
+
+            /*
+             * Esse é um comentario 
+             * de multiplas linhas...
+             */
+            Console.WriteLine("O C# tem o XML Comments");
+        }
+    }
+}
