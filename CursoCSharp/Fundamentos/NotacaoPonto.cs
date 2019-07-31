88b9234c00c0cf88e31788e5eef9c4724240acd8
LeMicheluti <lemicheluti@gmail.com>
2019-07-31 06:44:33 -0300

    Notação Ponto

---
+++ CursoCSharp/Fundamentos/NotacaoPonto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos {
+    class NotacaoPonto {
+        public static void Executar() {
+            string saudacao = "olá".ToUpper().Insert(3, "World!").Replace("World", " mundo");
+            Console.WriteLine(saudacao);
+
+            string valorImportante = null;
+            Console.WriteLine($"Valor = {valorImportante?.Length}");
+        }
+    }
+}
