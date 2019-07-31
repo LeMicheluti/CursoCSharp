026bbd47f65e7fb2193644257c2d83c46b1489e3
LeMicheluti <lemicheluti@gmail.com>
2019-07-31 06:45:30 -0300

    Formatando Números

---
+++ CursoCSharp/Fundamentos/FormatandoNumero.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace CursoCSharp.Fundamentos {
+    class FormatandoNumero {
+        public static void Executar() {
+            double valor = 15.175;
+
+            Console.WriteLine(valor.ToString("F1")); //mostra um caractere
+            Console.WriteLine(valor.ToString("C")); //valor monetario
+            Console.WriteLine(valor.ToString("P")); //Porcentagem
+            Console.WriteLine(valor.ToString("#.##")); //duas casas decimais
+
+            CultureInfo cultura = new CultureInfo("en-US");
+            Console.WriteLine(valor.ToString("C0", cultura)); //mostra o valor monetario em dolar 
+
+            int inteiro = 256;
+            Console.WriteLine(inteiro.ToString("D10")); //mostra o valor com 10 casas (zeros a esquerda)
+
+        }
+    }
+}
