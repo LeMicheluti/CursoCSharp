a95b74365f933fb24efdbd1979836e91164b702c
LeMicheluti <lemicheluti@gmail.com>
2019-07-31 06:37:31 -0300

    Init

---
+++ CursoCSharp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+using CursoCSharp.Fundamentos;
+
+namespace CursoCSharp {
+    class Program {
+        static void Main(string[] args) {
+            var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+                {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
+                {"Comentários - Fundamentos", Comentários.Executar},
+                {"Váriaveis e Constantes - Fundamentos", VariaveisEConstanstes.Executar},
+                {"Inferências - Fundamentos", Inferencia.Executar},
+                {"Interpolção - Fundamentos", Interpolacao.Executar},
+                {"Notação Ponto - Fundamentos", NotacaoPonto.Executar},
+                {"Lendo Dados - Fundamentos", LendoDados.Executar},
+                {"Formatando Números - Fundamentos", FormatandoNumero.Executar},
+                {"Conversões- Fundamentos", Conversoes.Executar},
+            });
+
+            central.SelecionarEExecutar();
+        }
+    }
+}
