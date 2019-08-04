eeec13a107d4937d1ba7e80a7422d7f412a23e82
LeMicheluti <lemicheluti@gmail.com>
2019-08-04 11:07:53 -0300

    Operadores Lógicos

---
+++ CursoCSharp/Fundamentos/OperadoresLogicos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos {
+    class OperadoresLogicos {
+        public static void Executar() {
+            var executouTrabalho1 = true;
+            var executouTrabalho2 = false;
+
+            var comprouTV50 = executouTrabalho1 && executouTrabalho2;
+            Console.WriteLine($"Comprou a TV 50? {comprouTV50}");
+
+            var comprouSorvete = executouTrabalho1 || executouTrabalho2;
+            Console.WriteLine($"Comprou sorverte? {comprouSorvete}");
+
+            bool comprouTV32 = executouTrabalho1 ^ executouTrabalho2;
+            Console.WriteLine($"Comprou TV 32? {comprouTV32}");
+
+            Console.WriteLine("Mair Saldável? {0}", !comprouSorvete);
+        }
+    }
+}
