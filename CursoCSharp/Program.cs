f6f190aee7a8c5456df82d277cd3ef8f2355d160
LeMicheluti <lemicheluti@gmail.com>
2019-08-19 06:55:07 -0300

    Operador Ternário

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -21,7 +21,7 @@ namespace CursoCSharp {
                 {"Operadores Lógicos - Fundamentos", OperadoresLogicos.Executar},
                 {"Operadores de Atribuição - Fundamentos", OperadoresAtribuicao.Executar},
                 {"Operadores Unários - Fundamentos", OperadoresUnarios.Executar},
-                {"Operadore Ternário - Fundamentos", OperadorTernario.Executar},
+                {"Operador Ternário - Fundamentos", OperadorTernario.Executar},
             });
 
             central.SelecionarEExecutar();
