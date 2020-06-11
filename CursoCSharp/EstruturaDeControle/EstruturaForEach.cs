a5a6a0a2eee73cea13cb3f4e541b3affe65d978c
LeMicheluti <lemicheluti@gmail.com>
2020-06-11 11:51:03 -0300

    Estrutura ForEach

---
+++ CursoCSharp/EstruturaDeControle/EstruturaForEach.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CursoCSharp.EstruturaDeControle
+{
+    class EstruturaForEach
+    {
+        public static void Executar()
+        {
+            var palavara = "Opa!";
+
+            foreach (var letra in palavara)
+            {
+                Console.WriteLine(letra);
+            }
+
+            var alunos = new string[] { "Ana", "Bia", "Carlos" };
+            foreach (var aluno in alunos)
+            {
+                Console.WriteLine(aluno);
+            }
+        }
+    }
+}
