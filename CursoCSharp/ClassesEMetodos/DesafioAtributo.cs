e72c877a29fa9e2b36c603936db0d704030e0379
LeMicheluti <lemicheluti@gmail.com>
2020-06-16 07:57:13 -0300

    Desafio Atributo

---
+++ CursoCSharp/ClassesEMetodos/DesafioAtributo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CursoCSharp.ClassesEMetodos
+{
+    class DesafioAtributo
+    {
+        int a = 10; //Atributo de instância
+        public static void Executar()
+        {
+            // Acessar variável 'a' dentro do método executar!
+            DesafioAtributo desafio = new DesafioAtributo();
+            Console.WriteLine(desafio.a);
+        }
+    }
+}
