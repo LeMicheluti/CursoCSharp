6c16795db6c4d55c565f1d7c1a2c3f3306cc66d4
LeMicheluti <lemicheluti@gmail.com>
2020-06-18 07:17:04 -0300

    Enumeracoes (Enum)

---
+++ CursoCSharp/ClassesEMetodos/ExemploEnum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CursoCSharp.ClassesEMetodos
+{
+    public enum Genero { Acao, Aventura, Terror, Animacao, Comedia };
+
+    public class Filme
+    {
+        public string Titulo;
+        public Genero GeneroDoFilme;
+    }
+    class ExemploEnum
+    {
+        public static void Executar()
+        {
+            int id = (int)Genero.Animacao;
+            Console.WriteLine(id);
+
+            var filme = new Filme();
+            filme.Titulo = "Sharknado 17";
+            filme.GeneroDoFilme = Genero.Comedia;
+
+            Console.WriteLine("{0} é {1}!", filme.Titulo, filme.GeneroDoFilme);
+        }
+    }
+}
