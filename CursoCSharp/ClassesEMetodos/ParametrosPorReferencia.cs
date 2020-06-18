d20071ec607d05b3496463f0253114800dca5f81
LeMicheluti <lemicheluti@gmail.com>
2020-06-18 07:59:52 -0300

    Parametros por referencia (ref/out)

---
+++ CursoCSharp/ClassesEMetodos/ParametrosPorReferencia.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CursoCSharp.ClassesEMetodos
+{
+    class ParametrosPorReferencia
+    {
+        public static void AlterarRef(ref int numero)
+        {
+            numero = numero + 1000;
+        }
+
+        public static void ALterarOut(out int numero) // out é unidirecional
+        {
+            numero = 15;
+        }
+
+        public static void Executar()
+        {
+            int a = 3;
+            AlterarRef(ref a);
+            Console.WriteLine(a);
+
+            ALterarOut(out int b);
+            Console.WriteLine(b);
+        }
+    }
+}
