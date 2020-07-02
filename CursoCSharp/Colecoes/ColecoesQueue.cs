3a5470a1617813c280f525d6c541c4851c9d47ae
LeMicheluti <lemicheluti@gmail.com>
2020-07-02 07:36:45 -0300

    Queue

---
+++ CursoCSharp/Colecoes/ColecoesQueue.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CursoCSharp.Colecoes
+{
+    class ColecoesQueue
+    {
+        public static void Executar()
+        {
+            var fila = new Queue<string>();
+
+            fila.Enqueue("A");
+            fila.Enqueue("B");
+            fila.Enqueue("C");
+
+            // Mostra o primeiro valor da fila sem retirar da fila
+            Console.WriteLine(fila.Peek());
+            Console.WriteLine(fila.Count);
+
+            // Mostra e retira o primeiro valor da fila
+            Console.WriteLine(fila.Dequeue());
+            Console.WriteLine(fila.Count);
+
+            foreach (var item in fila)
+            {
+                Console.WriteLine(item);
+            }
+
+            var salada = new Queue();
+            salada.Enqueue(3);
+            salada.Enqueue("Item");
+            salada.Enqueue(true);
+            salada.Enqueue(3.14);
+
+            Console.WriteLine(salada.Contains("item"));
+            Console.WriteLine(salada.Contains("Item"));
+        }
+    }
+}
