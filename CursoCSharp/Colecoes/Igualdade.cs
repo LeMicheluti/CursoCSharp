3da6981815517786c9fc5c8128a57d78541372a2
LeMicheluti <lemicheluti@gmail.com>
2020-07-02 07:56:50 -0300

    Igualdade (Equals e GetHashCode)

---
+++ CursoCSharp/Colecoes/Igualdade.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Colecoes
+{
+    class Igualdade
+    {
+        public static void Executar()
+        {
+            var p1 = new Produto("Caneta", 1.89);
+            var p2 = new Produto("Caneta", 1.89);
+            var p3 = p2;
+
+            // Compara o endereço de memória
+            Console.WriteLine(p1 == p2); // false
+            Console.WriteLine(p3 == p2); // true
+
+            // Override metodo Equals para comparar os valores dos atributos
+            Console.WriteLine(p1.Equals(p2)); // true
+
+        }
+    }
+}
