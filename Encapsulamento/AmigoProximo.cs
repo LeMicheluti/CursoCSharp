56465d7856b79e15353b2b3c86cfec5466b05ec1
LeMicheluti <lemicheluti@gmail.com>
2020-07-15 07:50:26 -0300

    Encapsulamento

---
+++ Encapsulamento/AmigoProximo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Encapsulamento
+{
+    public class AmigoProximo
+    {
+        public readonly SubCelebridade amigo = new SubCelebridade();
+
+        public void MeusAcessos()
+        {
+            Console.WriteLine("AmigoProximo...");
+
+            Console.WriteLine(amigo.InfoPublica);
+            //Console.WriteLine(amigo.CorDoOlho);
+            Console.WriteLine(amigo.NumeroCelular);
+            Console.WriteLine(amigo.JeitoDeFalar);
+            //Console.WriteLine(amigo.SegredoFamilia);
+            //Console.WriteLine(amigo.UsaMuitoPhotoshop);
+        }
+    }
+}
---
+++ Encapsulamento/SubCelebridade.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Encapsulamento
+{
+    public class SubCelebridade
+    {
+        // Todos
+        public string InfoPublica = "Tenho um instagram!";
+
+        // Herança
+        protected string CorDoOlho = "Verde";
+
+        // Mesmo Projeto (Assembly)
+        internal ulong NumeroCelular = 5511999999999;
+
+        // Herança ou mesmo projeto
+        protected internal string JeitoDeFalar = "Uso muitas gírias";
+
+        // Mesma class ou herança no memso projeto
+        private protected string SegredoFamilia = "Bla bla";
+
+        // Private é o padrão
+        bool UsaMuitoPhotoshop = true;
+
+        public void MeusAcessos()
+        {
+            Console.WriteLine("SubCelebridade...");
+
+            Console.WriteLine(InfoPublica);
+            Console.WriteLine(CorDoOlho);
+            Console.WriteLine(NumeroCelular);
+            Console.WriteLine(JeitoDeFalar);
+            Console.WriteLine(SegredoFamilia);
+            Console.WriteLine(UsaMuitoPhotoshop);
+        }
+    }
+}
