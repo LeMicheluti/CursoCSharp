6631b20b66939474cf680d84d9ef347618d8259f
LeMicheluti <lemicheluti@gmail.com>
2020-07-18 11:34:13 -0300

    Interface

---
+++ CursoCSharp/OO/Interface.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+
+namespace CursoCSharp.OO
+{
+    interface IOperacaoBinaria
+    {
+        int Operacao(int a, int b);
+    }
+
+    class Soma : IOperacaoBinaria
+    {
+        public int Operacao(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+    class Subtracao : IOperacaoBinaria
+    {
+        public int Operacao(int a, int b)
+        {
+            return a - b;
+        }
+    }
+
+    class Multiplicacao : IOperacaoBinaria
+    {
+        public int Operacao(int a, int b)
+        {
+            return a * b;
+        }
+    }
+
+    class Calculadora
+    {
+        List<IOperacaoBinaria> operacoes = new List<IOperacaoBinaria>
+        {
+            new Soma(),
+            new Subtracao(),
+            new Multiplicacao()
+        };
+
+        public string ExecutarOperacoes(int a, int b)
+        {
+            string resultado = "";
+
+            foreach (var op in operacoes)
+            {
+                resultado += $"Usando {op.GetType().Name} = {op.Operacao(a, b)}\n";
+            }
+
+            return resultado;
+        }
+    }
+
+    class Interface
+    {
+        public static void Executar()
+        {
+            var calc = new Calculadora();
+            var resultado = calc.ExecutarOperacoes(20, 5);
+            System.Console.WriteLine(resultado);
+        }
+    }
+}
