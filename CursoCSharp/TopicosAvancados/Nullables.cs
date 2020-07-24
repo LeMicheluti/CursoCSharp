aebe6c965724d805ea8b346051ecb44107df50da
LeMicheluti <lemicheluti@gmail.com>
2020-07-24 07:21:21 -0300

    Nullables

---
+++ CursoCSharp/TopicosAvancados/Nullables.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CursoCSharp.TopicosAvancados
+{
+    class Nullables
+    {
+        public static void Executar()
+        {
+            Nullable<int> num1 = null;
+            int? num2 = null;
+
+            if (num1.HasValue)
+            {
+                Console.WriteLine("Valor de num: {0}", num1);
+            }
+            else
+            {
+                Console.WriteLine("A variável não possui valor");
+            }
+
+            int valor = num1 ?? 1000;
+            Console.WriteLine(valor);
+
+            bool? booleana = null;
+            bool resuldado = booleana.GetValueOrDefault();
+            Console.WriteLine(resuldado);
+
+            try
+            {
+                int x = num1.Value;
+                int y = num2.Value;
+                Console.WriteLine(x + y);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
